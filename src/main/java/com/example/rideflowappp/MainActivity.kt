f1d3c5b55ac709e281638a0f10723622b2fca374
heyhusn <mhusnainaslam96@gmail.com>
2025-10-26 14:50:45 +0500

    Second Commit

---
+++ src/main/java/com/example/rideflowappp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.rideflowappp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val lottieView = findViewById<LottieAnimationView>(R.id.lottieView)
+        lottieView.playAnimation()
+
+        val thread = Thread {
+            try {
+                Thread.sleep(5000)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            } finally {
+                val intent = Intent(this@MainActivity, Screen2::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        thread.start()
+    }
+}
